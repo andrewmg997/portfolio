d938714f1b0e7fb13f9d6dfaef3235ae1d52b57f
NHaefelin <norina@haefelin.com>
2025-09-09 09:30:17 -0400

    Script for creating the plot

---
+++ Script.R
@@ -0,0 +1,11 @@
+#creating a plot for collab
+
+library(EVR628tools)
+
+library(tidyverse)
+
+data(data_lionfish)
+
+ggplot(data_lionfish,
+       aes(x = depth_m, y = total_weight_gr)) +
+  geom_point()
